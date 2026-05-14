64d7b5f9678b6769f118b27f2bd5caed0f50cc11
Brian Sheldon <brian.sheldon@live.ca>
2026-05-14 11:22:16 -0700

    keyboard and display

--- src/emu-esp32/emu-esp32.ino
+++ src/emu-esp32/emu-esp32.ino
@@ -15,6 +15,7 @@ void setup() {
   auto cfg = M5.config();
   M5Cardputer.begin(cfg);
   
+  // 240 x 135
   M5Cardputer.Display.setRotation(1); // Landscape
   M5Cardputer.Display.fillScreen( BLACK );
   M5Cardputer.Display.setCursor( 5, 5 );
@@ -22,6 +23,8 @@ void setup() {
   M5Cardputer.Display.setTextSize( 6 );
   //M5Cardputer.Display.setTextScroll(true);
   M5.Display.println( "CP/M" );
+  M5Cardputer.Display.setTextSize( 2 );
+  M5Cardputer.Display.setCursor( 2, 100 );
 
   Serial.begin( 115200 );
   delay( 2000 );
@@ -37,12 +40,43 @@ void setup() {
   cliSetup();
 }
 
+String keyData = "";
 void loop() {
   M5Cardputer.update();
   utilLoop();
   ioLoop();
   //loopEmu();
   cpu_frame();
+  if ( M5Cardputer.Keyboard.isChange() ) {
+    if ( M5Cardputer.Keyboard.isPressed() ) {
+      String nowData = "";
+      Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
+      for (auto i : status.word) {
+        nowData += i;
+      }
+      keyData += nowData;
+      if (status.del) {
+        if ( keyData.length() > 2 ) keyData.remove( keyData.length() - 1 );
+        cursorLeft();
+        cursorSave();
+        print( " " );
+        cursorUnsave();
+      }
+      if ( status.enter ) {
+        println();
+        cmdLine( keyData );
+        doPrompt();
+        M5Cardputer.Display.fillRect( 2, 100, 135, 240, BLACK );
+        M5.Display.print( "          " );
+        M5Cardputer.Display.setCursor( 2, 100 );
+        M5.Display.print( ">>> " );
+        keyData = "";
+      }
+      print( nowData );
+      M5.Display.print( nowData );
+      //keyData = "";
+    }
+  }
   delay( 1 );
 }
 
